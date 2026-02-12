4fbc334ef6893de0e32d61656afff8af2783a5ca
Martin Walters <Martybus@gmail.com>
2026-02-12 23:26:11 +1100

    troubleshooting env var v1

--- apps/web/src/app/campaigns/page.tsx
+++ apps/web/src/app/campaigns/page.tsx
@@ -5,6 +5,12 @@ import { supabaseBrowser } from "@/lib/supabaseBrowser";
 import { Frame } from "@/components/Frame";
 import { Card } from "@/components/Card";
 
+useEffect(() => {
+  console.log("SUPABASE_URL", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("HAS_ANON_KEY", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+}, []);
+
+
 type Template = {
   id: string;
   name: string;
