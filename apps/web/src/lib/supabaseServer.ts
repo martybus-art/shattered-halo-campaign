d6fa1d65bb3aa872590714bba66504cac24f5341
Martin Walters <Martybus@gmail.com>
2026-02-13 15:23:19 +1100

    changed supabaseServer.ts Key

--- apps/web/src/lib/supabaseServer.ts
+++ apps/web/src/lib/supabaseServer.ts
@@ -16,6 +16,6 @@ export function supabaseServer() {
 
 export function supabaseService() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   return createClient(url, key);
 }
